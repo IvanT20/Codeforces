1803b1afdbe3b68b5f4914866d96a04c6574333e
IvanT20 <90736704+IvanT20@users.noreply.github.com>
2024-02-06 21:59:27 -0500

    Add files via upload

---
+++ solutions/AFootball.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main() 
+{
+    int n;
+    int counter = 0;
+    int max = 0;
+    std::string team;
+    std::string bestTeam;
+    
+    std::cin >> n;
+
+    std::string arr[n];
+
+    for (int i = 0; i < n; ++i) 
+    {
+        std::cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        team = arr[i];
+
+        for (int j = 0; j < n; ++j)
+        {
+            if (arr[j] == team)
+            {
+                counter++;
+            }
+            
+            if (counter > max)
+            {
+                max = counter;
+                bestTeam = team;
+            }
+        }
+        counter = 0;
+    }
+    
+    std::cout << bestTeam;
+    return 0;
+}
